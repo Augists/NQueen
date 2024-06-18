2646f5811ea0743e712c653a4ec84d8bc62eb227
Augists on Arch <augists@duck.com>
2024-06-18 16:01:22 +0800

    feat&fix: lazy init for NDDFalse & fix True and False check & fix ref and deref & fix NOT & fix toBDD

--- src/main/java/bddvector/NDD.java
+++ src/main/java/bddvector/NDD.java
@@ -35,24 +35,17 @@ public class NDD {
                 vector.add(1);
             }
         }
-        // for (ArrayList<Integer> vector : NDDFalse.vectors) {
-        // for (int i = 1; i < fieldNum; i++) {
-        // vector.add(1);
-        // }
-        // vector.add(0);
-        // }
+        for (ArrayList<Integer> vector : NDDFalse.vectors) {
+            for (int i = 0; i < fieldNum; i++) {
+                vector.add(0);
+            }
+        }
     }
 
     public static void SetUpperBound(int[] upper) {
-        // System.out.println("===========set upper bound============");
-        // System.out.println("upper bound");
-        // for (int i = 0; i < upper.length; i++) {
-        //     System.out.print(upper[i] + " ");
-        // }
-        // System.out.println();
-
         upperBound = upper;
         int varNum = 0;
+        // if (NDDTCWrapper.useToBDD) {
         boolean useToBDD = true;
         if (useToBDD) {
             varNum = upper[fieldNum - 1] + 1;
@@ -63,18 +56,11 @@ public class NDD {
                     varNum = upper[i] - upper[i - 1];
             }
         }
-        // System.out.println("varNum " + varNum);
         div = new double[fieldNum];
         div[0] = Math.pow(2.0, varNum - upper[0] - 1);
         for (int i = 1; i < fieldNum; i++) {
             div[i] = Math.pow(2.0, varNum - (upper[i] - upper[i - 1]));
         }
-
-        // System.out.println("div");
-        // for (int i = 0; i < div.length; i++) {
-        //     System.out.print(div[i] + " ");
-        // }
-        // System.out.println();
     }
 
     public NDD() {
@@ -82,14 +68,9 @@ public class NDD {
     }
 
     public NDD(boolean flag) {
-        // field = fieldNum + 1;
         vectors = new HashSet<>();
         ArrayList<Integer> vector;
-        if (flag) {
-            vector = new ArrayList<Integer>();
-        } else {
-            vector = new ArrayList<Integer>();
-        }
+        vector = new ArrayList<Integer>();
         vectors.add(vector);
     }
 
@@ -110,18 +91,55 @@ public class NDD {
     }
 
     public boolean is_True() {
-        return this == NDDTrue;
+        // return this == NDDTrue;
+        if (this == NDDTrue) {
+            return true;
+        }
+        for (ArrayList<Integer> vector : vectors) {
+            for (int i = 0; i < vector.size(); i++) {
+                if (vector.get(i) != 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public boolean is_False() {
-        return this == NDDFalse;
+        // do not only use pointer compare to judge NDDFalse
+        if (this == NDDFalse) {
+            return true;
+        }
+        // only if every vector has 0 can be regarded as False
+        for (ArrayList<Integer> vector : vectors) {
+            boolean flag = false;
+            for (int i = 0; i < vector.size(); i++) {
+                if (vector.get(i) == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+                return false;
+        }
+        return true;
     }
 
     public static NDD ref(NDD ndd) {
+        for (ArrayList<Integer> vector : ndd.vectors) {
+            for (int i = 0; i < vector.size(); i++) {
+                bdd.ref(vector.get(i));
+            }
+        }
         return ndd;
     }
 
     public static void deref(NDD ndd) {
+        for (ArrayList<Integer> vector : ndd.vectors) {
+            for (int i = 0; i < vector.size(); i++) {
+                bdd.deref(vector.get(i));
+            }
+        }
     }
 
     public static NDD andTo(NDD a, NDD b) {
@@ -226,14 +244,27 @@ public class NDD {
             return NDDFalse;
         if (a.is_False())
             return NDDTrue;
-        NDD ret = new NDD();
+
+        NDD ret = NDD.NDDTrue;
+
         for (ArrayList<Integer> vector : a.vectors) {
+            NDD tmp = new NDD();
             for (int i = 0; i < vector.size(); i++) {
-                ArrayList<Integer> temp = new ArrayList<>(vector); // not 1 1 1 1
-                temp.set(i, bdd.not(vector.get(i)));
-                ret.vectors.add(temp);
+                if (vector.get(i) == 1)
+                    continue;
+                ArrayList<Integer> temp = new ArrayList<>(); // not 1 1 1 1
+                for (int j = 0; j < vector.size(); j++) {
+                    temp.add(1);
+                }
+                temp.set(i, bdd.ref(bdd.not(vector.get(i))));
+                tmp.vectors.add(temp);
             }
+            NDD t = ret;
+            ret = NDD.ref(NDD.AND(ret, tmp));
+            NDD.deref(t);
+            NDD.deref(tmp);
         }
+
         return ret;
     }
 
@@ -303,10 +334,7 @@ public class NDD {
             for (int i = vector.size() - 1; i >= 0; i--) {
                 int idx = vector.get(i);
                 int temp = lastIdx;
-                lastIdx = bdd.and(idx, lastIdx);
-                // System.out.println("last idx " + lastIdx);
-                // bdd.printDot("/data/augists/NQueen/lastidx"+lastIdx, lastIdx);
-                bdd.ref(lastIdx);
+                lastIdx = bdd.ref(bdd.and(idx, lastIdx));
                 bdd.deref(temp);
             }
             bdds.add(lastIdx);
@@ -331,20 +359,7 @@ public class NDD {
     }
 
     private static NDD toNDDFunc(int a) {
-        // if (satCountTest) {
-        // System.out.println("=======to bdd vector=======");
-        // // System.out.println("root " + a);
-        // }
-
         HashMap<Integer, HashMap<Integer, Integer>> decomposed = decompose(a);
-
-        // System.out.println("decompose" + decomposed);
-        // for (Map.Entry<Integer, HashMap<Integer, Integer>> entry :
-        // decomposed.entrySet()) {
-        // for (Map.Entry<Integer, Integer> i : entry.getValue().entrySet()) {
-        // bdd.printDot("/data/augists/NQueen/idx_"+i.getValue(), i.getValue());
-        // }
-        // }
 
         ArrayList<Integer> temp = new ArrayList<>();
         Set<ArrayList<Integer>> vectors = new HashSet<>();
@@ -354,8 +369,6 @@ public class NDD {
         }
 
         toBDDvectorDFS(vectors, decomposed, a, temp);
-
-        // System.out.println("vectors " + vectors);
 
         return new NDD(vectors);
     }
@@ -370,8 +383,6 @@ public class NDD {
         HashMap<Integer, Integer> map = data.get(root);
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             int end = entry.getKey();
-
-            // System.out.println("end level " + bdd_getField(end));
 
             int idx = entry.getValue();
             int fieldDiff = bdd_getField(end) - bdd_getField(root) - 1;
@@ -414,8 +425,6 @@ public class NDD {
         ArrayList<HashSet<Integer>> boundary_points = new ArrayList<HashSet<Integer>>();
 
         get_boundary_tree(a, boundary_tree, boundary_points);
-        // System.out.println("boundary tree " + boundary_tree);
-        // System.out.println("boundary points " + boundary_points);
 
         for (int curr_level = 0; curr_level < fieldNum - 1; curr_level++) {
             for (int root : boundary_points.get(curr_level)) {
@@ -566,8 +575,9 @@ public class NDD {
             NDD n = entry.getKey();
             int b = entry.getValue();
             for (ArrayList<Integer> vector : n.vectors) {
-                vector.set(field, b);
-                ret.vectors.add(vector);
+                ArrayList<Integer> temp = new ArrayList<>(vector);
+                temp.set(field, b);
+                ret.vectors.add(temp);
             }
         }
         return ret;
@@ -595,16 +605,6 @@ public class NDD {
     }
 
     private static double satCountRec(NDD curr) {
-        // if (satCountTest) {
-        // System.out.println("===========sat count==============");
-        // System.out.println("fieldNum: " + fieldNum);
-        // System.out.println("div");
-        // for (int i = 0; i < fieldNum; i++) {
-        // System.out.print(div[i] + " ");
-        // }
-        // System.out.println();
-        // // System.out.println("vectors: " + curr.vectors);
-        // }
         double ret = 0.0;
         ArrayList<Double> retList = new ArrayList<>();
         for (ArrayList<Integer> vector : curr.vectors) {
@@ -612,7 +612,6 @@ public class NDD {
             for (int i = fieldNum - 1; i >= 0; i--) {
                 subRet = subRet * bdd.satCount(vector.get(i)) / div[i];
             }
-            // System.out.println("subRet: " + subRet);
             retList.add(subRet);
         }
         for (double count : retList) {
